64cfdcf60f7a848a3926835dfc992c719542a132
Martin Atkins <mart@degeneration.co.uk>
2017-10-05 18:06:19 -0700

    core: add schema argument to EvalContext.Interpolate
    The schema may be nil if we don't know it, in which case the normal
    interpolation codepath will still work just fine but HCL2-based
    interpolation will not.
    
    Over subsequent commits we will gradually set this argument in more cases
    until we have it set comprehensively for all calls.

--- terraform/eval_interpolate.go
+++ terraform/eval_interpolate.go
@@ -4,19 +4,21 @@ import (
 	"log"
 
 	"github.com/hashicorp/terraform/config"
+	"github.com/hashicorp/terraform/config/configschema"
 )
 
 // EvalInterpolate is an EvalNode implementation that takes a raw
 // configuration and interpolates it.
 type EvalInterpolate struct {
 	Config        *config.RawConfig
+	Schema        *configschema.Block
 	Resource      *Resource
 	Output        **ResourceConfig
 	ContinueOnErr bool
 }
 
 func (n *EvalInterpolate) Eval(ctx EvalContext) (interface{}, error) {
-	rc, err := ctx.Interpolate(n.Config, n.Resource)
+	rc, err := ctx.Interpolate(n.Config, n.Resource, n.Schema)
 	if err != nil {
 		if n.ContinueOnErr {
 			log.Printf("[WARN] Interpolation %q failed: %s", n.Config.Key, err)
